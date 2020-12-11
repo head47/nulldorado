e7cd2cca6a0461b5fadf37dfe4ffbce1c947faae
head47 <sanya@national.shitposting.agency>
2020-12-11 12:24:44 +0300

    миграция на mysql

--- nulldorado/settings.py
+++ nulldorado/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'nulldorado.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nulldorado_data',
+        'USER': "djangouser",
+        'PASSWORD': 'verysecurepassword'
     }
 }
 
