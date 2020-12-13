89b602f16228b933497f8e14b88d9e1fddc20d17
head47 <sanya@national.shitposting.agency>
2020-12-13 10:29:56 +0300

    каталог (1 уровень)

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('catalog', views.catalogue, name='catalogue'),
     path('', views.index, name='index'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from shop.models import Item
+from shop.models import Item, Category, Subcategory
 import random
+from collections import OrderedDict
 
 def index(request):
     newItems = list(Item.objects.filter(new=True))
@@ -12,5 +13,18 @@ def index(request):
     template = loader.get_template('shop/index.html')
     context = {
         'showcase': showcase,
+    }
+    return HttpResponse(template.render(context, request))
+
+def catalogue(request):
+    links = OrderedDict()
+    categories = list(Category.objects.all())
+    for i in categories:
+        subcategories = Subcategory.objects.filter(parent__id=i.id)
+        links[i] = list(subcategories)
+    template = loader.get_template('shop/assort.html')
+    context = {
+        #'categories': categories,
+        'links': links
     }
     return HttpResponse(template.render(context, request))
