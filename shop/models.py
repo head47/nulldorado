aaad4e15437be417ca93fd9f3d28305ec6d1e306
head47 <sanya@national.shitposting.agency>
2020-12-13 09:33:27 +0300

    главная страница с рандомом

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    picture = models.CharField(max_length=32)
+    def __str__(self):
+        return self.name
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=64)
+    parent = models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=64)
+    parent = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    picture = models.CharField(max_length=32)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    new = models.BooleanField(default=False)
+    available = models.PositiveIntegerField(default=0)
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    phone = models.CharField(max_length=16)
+    email = models.EmailField()
+    items = models.JSONField()
--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from shop.models import Item
+import random
+
 def index(request):
+    newItems = list(Item.objects.filter(new=True))
+    showcase = random.sample(newItems, 3)
     template = loader.get_template('shop/index.html')
-    context = {}
+    context = {
+        'showcase': showcase,
+    }
     return HttpResponse(template.render(context, request))
