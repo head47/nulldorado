0a32e44fc95aca21ea7c1b38734d7f985eec6a7d
head47 <sanya@national.shitposting.agency>
2020-12-13 12:55:13 +0300

    failover

--- nulldorado/router.py
+++ nulldorado/router.py
@@ -1,11 +1,41 @@
 import random
+from django.conf import settings
+import MySQLdb
+
+def dbUp(database_name):
+    try:
+        db_definition = getattr(settings, 'DATABASES')[database_name]
+        db = MySQLdb.connect(host=db_definition['HOST'],
+            port=int(db_definition['PORT']),
+            user=db_definition['USER'],
+            passwd=db_definition['PASSWORD'],
+            db=db_definition['NAME'])
+        db.close()
+        return True
+    except Exception as e:
+        print('Database down:',database_name)
+        return False
 
 class dbRouter:
     def db_for_read(self, model, **hints):
-        return random.choice(['default', 'node02', 'node03'])
+        dblist = ['default', 'node02', 'node03']
+        choice = random.choice(dblist)
+        while not dbUp(choice):
+            dblist.remove(choice)
+            choice = random.choice(dblist)
+            if len(dblist) == 1:
+                break
+        return choice
 
     def db_for_write(self, model, **hints):
-        return random.choice(['default', 'node02', 'node03'])
+        dblist = ['default', 'node02', 'node03']
+        choice = random.choice(dblist)
+        while not dbUp(choice):
+            dblist.remove(choice)
+            choice = random.choice(dblist)
+            if len(dblist) == 1:
+                break
+        return choice
 
     def allow_relation(self, obj1, obj2, **hints):
         db_set = {'default', 'node02', 'node03'}
