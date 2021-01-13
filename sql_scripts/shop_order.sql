b3dce11978a1ee0a2703697ac376663a3534899d
head47 <sanya@national.shitposting.agency>
2021-01-13 16:37:51 +0300

    апдейт скриптов sql

--- sql_scripts/shop_order.sql
+++ sql_scripts/shop_order.sql
@@ -1,3 +1,2 @@
-INSERT INTO nulldorado_data.shop_order (phone,email,items,order_status) VALUES
-	 ('123456789',qwe1@qwe.qwe,{'laptop-1':'123'},'V'),
-     ('123456789',qwe1@qwe.qwe,{'laptop-2':'456'},'V');
+INSERT INTO nulldorado_data.shop_order (phone,email,items,order_status,address) VALUES
+	 ('+71234567890','example@example.com','{"6": 1}','N','Ломоносова, 9');
